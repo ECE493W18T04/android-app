1017b81c278fb3e476c4fe80045ec38497bae331
Curtis Malainey <curtis@malainey.com>
2018-02-27 23:04:27 -0700

    Complete untested brightness algorithm

--- src/AutoBrightnessAlg.cpp
+++ src/AutoBrightnessAlg.cpp
@@ -1,20 +1,53 @@
 #include "AutoBrightnessAlg.h"
 
 #define DEFAULT_INSTANCE 0.1
+#define MIN_STATE 1
+#define MAX_STATE 100
+#define MIN_BRIGHTNESS 0 // these will need to be updated with reasonable values
+#define MAX_BRIGHTNESS 1024
+#define HYSTERISIS_THRESHOLD 2 //update
+#define LOCK_TICK_THRESHOLD 16
+#define DEFAULT_ALPHA 0.5
 
-AutoBrightnessAlg::AutoBrightnessAlg() _alpha(DEFAULT_ALPHA) {}
+AutoBrightnessAlg::AutoBrightnessAlg() : _alpha(DEFAULT_ALPHA) {}
 
-AutoBrightnessAlg::AutoBrightnessAlg(double alpha) _alpha(alpha) {}
+AutoBrightnessAlg::AutoBrightnessAlg(double alpha) : _alpha(alpha) {}
 
-void AutoBrightnessAlg:addSample(uint32_t dataSample) {
+void AutoBrightnessAlg::addSample(uint32_t dataSample) {
     // exponential decay to smooth out the value
     int8_t new_brightness_state;
-    current_value = (current_value * (1 - _alpha)) + (dataSample * _alpha);
-    new_brightness_state = map(current_value, MIN_BRIGHTNESS, MAX_BRIGHTNESS, MIN_STATE, MAX_STATE);
-    if (abs(_current_state - new_brightness_state) > HYSTERISIS_THRESHOLD) {
+    _current_value = (_current_value * (1 - _alpha)) + (dataSample * _alpha);
+    new_brightness_state = map(_current_value, MIN_BRIGHTNESS, MAX_BRIGHTNESS, MIN_STATE, MAX_STATE);
+    new_brightness_state = max(min(new_brightness_state, MIN_STATE), MAX_STATE);
+    // handle hystersis
+    bool isLocked = _internal_state == LOCKED;
+    bool pastThreshold = abs(_current_state - new_brightness_state) > HYSTERISIS_THRESHOLD;
+    if (isLocked && pastThreshold) {
+        // we have moved outside out lock range, unlock
         _internal_state = UNLOCKED;
+        _ticks_in_current_state = 0;
+        _current_state = new_brightness_state;
+    } else if (!isLocked) {
+        if (pastThreshold) {
+            // we are still moving, update
+            _current_state = new_brightness_state;
+            _ticks_in_current_state = 0;
+        } else {
+            // we are still close enough
+            _ticks_in_current_state++;
+            if (_ticks_in_current_state > LOCK_TICK_THRESHOLD) {
+                // we have settled, lock value
+                _internal_state = LOCKED;
+            }
+        }
+
     }
-    // add state locking for hysterisis
+}
+
+// https://os.mbed.com/questions/54326/How-can-we-use-map-function
+long AutoBrightnessAlg::map(long x, long in_min, long in_max, long out_min, long out_max)
+{
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 uint8_t AutoBrightnessAlg::getState() {
--- src/AutoBrightnessAlg.h
+++ src/AutoBrightnessAlg.h
@@ -1,23 +1,30 @@
 #ifndef AUTOBRIGHTNESSALG_H
 #define AUTOBRIGHTNESSALG_H
 
-typedef uint8_t auto_brightness_state; enum {
-    UNLOCKED;
-    LOCKED;
-}
+#include <mbed.h>
 
-class AutoBrightnessAlgorithm {
+#define min(x, y) (x < y ? x : y)
+#define max(x, y) (x > y ? x : y)
+
+typedef uint8_t auto_brightness_state; enum {
+    UNLOCKED,
+    LOCKED,
+};
+
+class AutoBrightnessAlg {
 public:
-    AutoBrightnessAlgorithm();
-    AutoBrightnessAlgorithm(double alpha);
+    AutoBrightnessAlg();
+    AutoBrightnessAlg(double alpha);
     void addSample(uint32_t dataSample);
     uint8_t getState();
     void setVariability(double alpha);
 private:
+    long map(long x, long in_min, long in_max, long out_min, long out_max);
     double _alpha;
     uint32_t _current_value = 0;
     int8_t _current_state = 0;
     auto_brightness_state _internal_state = UNLOCKED;
-}
+    uint16_t _ticks_in_current_state = 0;
+};
 
 #endif
