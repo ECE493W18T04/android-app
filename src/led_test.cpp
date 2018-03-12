8cc7ca96fba8ac6a44c8d790b54f2dbbcdb78b2e
Curtis Malainey <curtis@malainey.com>
2018-03-12 11:10:57 -0600

    take over driver

--- src/led_test.cpp
+++ src/led_test.cpp
@@ -1,22 +1,39 @@
-// #include <mbed.h>
-// #include <APA102.h>
-//
-// APA102 matrix = APA102(p23, p24, p25, 200000);
-//
-// int main() {
-//     unsigned int data[256] = {0};
-//     for (unsigned char i = 0; i < 255; i++) {
-//         data[i] = 0xEF00FF00;
-//     }
-//     matrix.SetBuffer(data, 8, 32, 0, 0, true, false);
-//     matrix.Repaint();
-//     wait_ms(3000);
-//     while (1) {
-//         for (unsigned char i = 0; i < 255; i++) {
-//             data[1] = i << 8;
-//             matrix.SetBuffer(data, 8, 32, 0, 0, true, false);
-//             matrix.Repaint();
-//             wait_ms(10);
-//         }
-//     }
-// }
+#include <mbed.h>
+#include <APA102.h>
+
+// APA102 matrix = APA102(p23, p24, p25, 2000000);
+SPI _spi = SPI(p23, p24, p25);
+
+int main() {
+    unsigned int data[256] = {0};
+    _spi.format(8,3);
+    _spi.frequency(2000000);
+    for (int i = 0; i < 256; i++) {
+        data[i] = 0xE0000000;
+    }
+    // matrix.SetBuffer(data, 8, 32, 8, 0, true, false);
+    // matrix.Repaint();
+    while (1) {
+        for (uint16_t i = 0; i < 256; i++) {
+            data[i] = 0xEF0000FF;
+	    _spi.write(0X00);  // Start
+	    _spi.write(0X00);
+	    _spi.write(0X00);
+	    _spi.write(0X00);
+	    
+	    for(int index = 0; index < 256; index++)
+	    {
+		unsigned int val = data[index];
+		_spi.write((val>>24)&0xFF);  
+		_spi.write((val>>16)&0xFF);  
+		_spi.write((val>>8)&0xFF);  
+		_spi.write(val&0xFF);  
+	    }
+	    _spi.write(0XFF); // Stop
+	    _spi.write(0XFF);
+	    _spi.write(0XFF);
+	    _spi.write(0XFF);
+            wait_ms(100);
+        }
+    }
+}
