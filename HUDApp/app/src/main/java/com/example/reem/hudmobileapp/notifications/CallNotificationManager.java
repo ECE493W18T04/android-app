4a3d45de31a5e879dd4d23369f9e436b5ea638af
Navjeet Dhaliwal <nsd@ualberta.ca>
2018-03-13 12:39:08 -0600

    added some notification parsing code. not complete

--- HUDApp/app/src/main/java/com/example/reem/hudmobileapp/notifications/CallNotificationManager.java
+++ HUDApp/app/src/main/java/com/example/reem/hudmobileapp/notifications/CallNotificationManager.java
@@ -1,12 +1,42 @@
 package com.example.reem.hudmobileapp.notifications;
 
+import android.app.Notification;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
 /**
  * Created by Reem on 2018-03-09.
  */
 
 public class CallNotificationManager extends NotificationManager {
+    private StatusBarNotification n;
+    public CallNotificationManager(StatusBarNotification sbn) {
+        n=sbn;
+    }
+
+    private void ParseCallNotification() {
+        String pack = n.getPackageName();
+        String ticker ="";
+        if(n.getNotification().tickerText !=null) {
+            ticker = n.getNotification().tickerText.toString();
+        }
+        Bundle extras = n.getNotification().extras;
+        String title = extras.getString("android.title");
+        String text = extras.getCharSequence("android.text").toString();
+        int id1 = extras.getInt(Notification.EXTRA_SMALL_ICON);
+        Bitmap id = n.getNotification().largeIcon;
+
+
+        Log.d("Package",pack);
+        Log.d("Ticker",ticker);
+        Log.d("Title",title);
+        Log.d("Text",text);
+    }
     @Override
     public byte[] getContent() {
+        ParseCallNotification();
         return new byte[0];
     }
 }
--- HUDApp/app/src/main/java/com/example/reem/hudmobileapp/notifications/GoogleMapsNotificationManager.java
+++ HUDApp/app/src/main/java/com/example/reem/hudmobileapp/notifications/GoogleMapsNotificationManager.java
@@ -1,12 +1,50 @@
 package com.example.reem.hudmobileapp.notifications;
 
+import android.app.Activity;
+import android.app.Notification;
+import android.content.Context;
+import android.service.notification.NotificationListenerService;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.RemoteViews;
+import android.widget.TextView;
+
+import com.example.reem.hudmobileapp.activities.MainActivity;
+
 /**
  * Created by Reem on 2018-03-09.
  */
 
 public class GoogleMapsNotificationManager extends NotificationManager {
+    private final String DEBUG_TAG = this.getClass().getSimpleName();
+    private RelativeLayout layout;
+
+    public GoogleMapsNotificationManager(RelativeLayout rl) {
+        layout = rl;
+    }
     @Override
     public byte[] getContent() {
+        parseNotification(layout);
         return new byte[0];
+    }
+    private void parseNotification(RelativeLayout rl) {
+
+        ImageView turnArrow = (ImageView) rl.getChildAt(0);
+
+        //Log.d(DEBUG_TAG, Integer.toHexString(turnArrow.hashCode()));
+        //Log.d(DEBUG_TAG, Integer.toHexString(turnArrow.getDrawable().getConstantState().hashCode()));
+
+        LinearLayout ll = (LinearLayout) ((LinearLayout) rl.getChildAt(1)).getChildAt(0);
+        TextView distance = (TextView) ll.getChildAt(0);
+        //
+        TextView direction = (TextView) ll.getChildAt(1);
+        TextView eta = (TextView) ll.getChildAt(2);
+
+        Log.d(DEBUG_TAG, "Distance: "+distance.getText().toString());
+        Log.d(DEBUG_TAG, "Directions: "+direction.getText().toString());
+        Log.d(DEBUG_TAG, "ETA: "+eta.getText().toString());
     }
 }
