841e2b980b73055fdbddd86466973f22952fdbf1
Curtis Malainey <curtis@malainey.com>
2018-03-22 10:41:37 -0600

    more state machine headers

---
+++ src/VehicleSpeed.h
@@ -0,0 +1,15 @@
+#ifndef VEHICLESPEED_h
+#define VEHICLESPEED_h
+
+#include "DefaultState.h"
+
+class VehicleSpeed : DefaultState {
+public:
+    VehicleSpeed(StateManager& _stateMgr);
+    bool tick();
+    void update(uint8_t speed, uint8_t units);
+private:
+    uint8_t speed, units;
+};
+
+#endif // VEHICLESPEED_h
