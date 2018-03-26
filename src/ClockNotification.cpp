d19b0111aba740c4ce74b1d13851409cafebcee5
Curtis Malainey <curtis@malainey.com>
2018-03-26 16:22:38 -0600

    Add clock functions

--- src/ClockNotification.cpp
+++ src/ClockNotification.cpp
@@ -1,10 +1,15 @@
 #include "ClockNotification.h"
+#include "StateManager.h"
 
 ClockNotification::ClockNotification(StateManager& _stateMgr) : DefaultState(_stateMgr) {
     setActive(false);
 }
 
 bool ClockNotification::tick() {
+    char buffer[32];
+    time_t seconds = time(NULL);
+    // strftime(buffer, 32, "%I:%M %p\n", localtime(&seconds));
+    // printf("%s", buffer);
     return DefaultState::tick();
 }
 
