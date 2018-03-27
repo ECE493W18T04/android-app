80446ef4cd0eb1470e361a2c53e8dcab71f4a02f
Curtis Malainey <curtis@malainey.com>
2018-03-27 13:46:55 -0600

    Bitmaps and fuel level

--- src/FuelLevel.cpp
+++ src/FuelLevel.cpp
@@ -1,17 +1,36 @@
 #include "FuelLevel.h"
+#include "GraphicsManager.h"
+#include "StateManager.h"
 
 #define FUEL_LEVEL_TIME 50
+#define FUEL_LEVEL_INVALID 0xFF
 
-FuelLevel::FuelLevel(StateManager& _stateMgr) : TimedState(_stateMgr, FUEL_LEVEL_TIME) {
+const uint8_t pump[] = {0x3C, 0xA4, 0xA4, 0xBC, 0xBC, 0xFC, 0x3C, 0x7E};
+
+FuelLevel::FuelLevel(StateManager& _stateMgr) : TimedState(_stateMgr, FUEL_LEVEL_TIME), fuelLevel(FUEL_LEVEL_INVALID) {
 }
 
 void FuelLevel::update(uint8_t fuelLevel) {
+    // TODO handle only waking on multiples of 10
+    this->fuelLevel = fuelLevel;
 }
 
 bool FuelLevel::tick() {
+    GraphicsManager& gfx = getManager().getGfxManager();
+    gfx.erase();
+    gfx.drawBitmap(pump, 1, 0, 7, 8);
+    if (FUEL_LEVEL_INVALID == fuelLevel) {
+        gfx.placeText("N/A", 10);
+    } else {
+        char buffer[10];
+        sprintf(buffer, "%d%%", fuelLevel);
+        gfx.placeText(buffer, 10);
+    }
+    gfx.drawBuffer();
     return TimedState::tick();
 }
 
 bool FuelLevel::kick() {
+    resetTicks();
     return true;
 }
--- src/GraphicsManager.h
+++ src/GraphicsManager.h
@@ -14,7 +14,7 @@ public:
     void fill(uint8_t x, uint8_t y, uint8_t width, uint8_t height);
     int placeText(char text[], int horizonatalOffset);
     void eraseSection(uint8_t x, uint8_t y, uint8_t width, uint8_t height);
-    void drawBitmap(uint8_t bitmap[], uint8_t x, uint8_t y, uint8_t width, uint8_t length);
+    void drawBitmap(const uint8_t bitmap[], uint8_t x, uint8_t y, uint8_t width, uint8_t length);
     void drawBuffer();
     void setColor(uint16_t _hue, uint8_t _saturation);
 private:
