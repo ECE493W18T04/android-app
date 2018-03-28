71ac5e9db63512e15bd5bb0bf1fd1070b01c6c09
Curtis Malainey <curtis@malainey.com>
2018-03-27 18:29:18 -0600

    Add music note

--- src/MusicNotification.cpp
+++ src/MusicNotification.cpp
@@ -1,11 +1,20 @@
 #include "MusicNotification.h"
+#include "StateManager.h"
+#include "GraphicsManager.h"
 
 #define MUSICNOTIFICATION_TICKS 100
 
+const uint8_t note[] = {0x18, 0x68, 0x48, 0x58, 0xD8, 0xC0};
+
 MusicNotification::MusicNotification(StateManager& _stateMgr) : TimedState(_stateMgr, MUSICNOTIFICATION_TICKS) {
+    setActive(true);
 }
 
 bool MusicNotification::tick() {
+    GraphicsManager& gfx = getManager().getGfxManager();
+    gfx.erase();
+    gfx.drawBitmap(note, 0, 1, 5, 6);
+    gfx.drawBuffer();
     return TimedState::tick();
 }
 
