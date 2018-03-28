4b12a59bf8e905859816e48f6b136a4b122b4cca
Curtis Malainey <curtis@malainey.com>
2018-03-28 10:48:53 -0600

    Add phone

--- src/PhoneNotification.cpp
+++ src/PhoneNotification.cpp
@@ -1,9 +1,17 @@
 #include "PhoneNotification.h"
+#include "StateManager.h"
+#include "GraphicsManager.h"
+
+const uint8_t phone[] = {0xC0, 0xC0, 0x80, 0x80, 0x80, 0xC0, 0xC0};
 
 PhoneNotification::PhoneNotification(StateManager& _stateMgr) : TempState(_stateMgr) {
 }
 
 bool PhoneNotification::tick() {
+    GraphicsManager& gfx = getManager().getGfxManager();
+    gfx.erase();
+    gfx.drawBitmap(phone, 1, 0, 2, 7);
+    gfx.drawBuffer();
     return TempState::tick();
 }
 
