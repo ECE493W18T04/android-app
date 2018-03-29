52579fbf53af721d446ad5d1691469f7fca61f57
Curtis Malainey <curtis@malainey.com>
2018-03-29 16:27:03 -0600

    Fix timed states

--- src/TimedState.cpp
+++ src/TimedState.cpp
@@ -1,12 +1,16 @@
 #include "TimedState.h"
 
-TimedState::TimedState(StateManager& _stateMgr, int ticks) : State(_stateMgr) {
-}
+TimedState::TimedState(StateManager& _stateMgr, int ticks) : State(_stateMgr), resetTo(ticks), ticksLeft(ticks) {}
 
 bool TimedState::tick() {
-    // TODO
-    return false;
+    ticksLeft--;
+    if (ticksLeft < 0) {
+        setActive(false);
+        return false;
+    }
+    return true;
 }
 
 void TimedState::resetTicks() {
+    ticksLeft = resetTo;
 }
