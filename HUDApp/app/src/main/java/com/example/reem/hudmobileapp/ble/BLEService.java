1fdd7bc57d8c74d6da5faf7d86993734033bad55
rmaarouf <rmaarouf@ualberta.ca>
2018-03-29 13:43:16 -0600

    voice stuff

--- HUDApp/app/src/main/java/com/example/reem/hudmobileapp/ble/BLEService.java
+++ HUDApp/app/src/main/java/com/example/reem/hudmobileapp/ble/BLEService.java
@@ -54,6 +54,17 @@ public class BLEService extends Service {
     private CharacteristicWriter writer;
 
 
+    public final static String ACTION_GATT_CONNECTED =
+            "com.example.bluetooth.le.ACTION_GATT_CONNECTED";
+    public final static String ACTION_GATT_DISCONNECTED =
+            "com.example.bluetooth.le.ACTION_GATT_DISCONNECTED";
+    public final static String ACTION_GATT_SERVICES_DISCOVERED =
+            "com.example.bluetooth.le.ACTION_GATT_SERVICES_DISCOVERED";
+    public final static String ACTION_DATA_AVAILABLE =
+            "com.example.bluetooth.le.ACTION_DATA_AVAILABLE";
+    public final static String EXTRA_DATA =
+            "com.example.bluetooth.le.EXTRA_DATA";
+
     public IBinder getBinder() {
         return mBinder;
     }
