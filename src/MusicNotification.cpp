69b7f9f9f29d8eae3bcf272e5b2f3afb07845c82
Curtis Malainey <curtis@malainey.com>
2018-04-02 20:10:56 -0600

    50% increase to music timeout

--- src/MusicNotification.cpp
+++ src/MusicNotification.cpp
@@ -2,7 +2,7 @@
 #include "StateManager.h"
 #include "GraphicsManager.h"
 
-#define MUSICNOTIFICATION_TICKS 100
+#define MUSICNOTIFICATION_TICKS 150
 
 const uint8_t note[] = {0x18, 0x68, 0x48, 0x58, 0xD8, 0xC0};
 
